2dff78abad470cc15511e7e5ec9b54dab0d8a3d5
Lucas Melo <lucasmelo.tec.saude@gmail.com>
2025-10-02 20:23:39 -0300

    Adicionado o método DELETE

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 from fastapi import HTTPException
 from fastapi import status
@@ -40,6 +41,26 @@ async def post_curso(curso : Curso):
     del curso.id
     return curso
 
+@app.put('/cursos/{curso_id}')
+async def put_curso(curso_id: int, curso: Curso):
+    if curso_id in cursos:
+        cursos[curso_id] = curso
+        del curso.id
+
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Não existe curso com o id {curso_id}")
+
+@app.delete('/cursos/{curso_id}')
+async def delete_curso(curso_id: int):
+    if curso_id in cursos:
+        del cursos[curso_id]
+        return JSONResponse(status_code=status.HTTP_204_NO_CONTENT,
+                            content="Deletado com sucesso")
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Não existe curso com o id {curso_id}")
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
