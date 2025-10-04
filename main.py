8f22424281a56b2405533c6fff70d7da2cd9e3bb
Lucas Melo <lucasmelo.tec.saude@gmail.com>
2025-10-03 21:23:33 -0300

    Adicionado um Header Parameter para teste

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import List, Optional
 from fastapi.responses import JSONResponse
 from fastapi import Path
 from fastapi import Query
+from fastapi import Header
 
 from fastapi import FastAPI
 from fastapi import HTTPException
@@ -69,10 +70,12 @@ async def delete_curso(curso_id: int):
                             detail=f"Não existe curso com o id {curso_id}")
 
 @app.get('/calculadora')
-async def calcular(a : int = Query(default=None, gt=5), b : int = Query(default=None, gt=10), c : Optional[int] = None):
+async def calcular(a : int = Query(default=None, gt=5), b : int = Query(default=None, gt=10),
+                   x_geek : str = Header(default=None), c : Optional[int] = None):
     soma : int = a + b
     if c:
         soma += c
+    print(f'X-Geek: {x_geek}')
     return {"resultado" : soma}
 
 if __name__ == '__main__':
